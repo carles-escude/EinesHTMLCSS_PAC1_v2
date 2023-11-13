7c3ee5a6a4eb1d47d9f859383cd41cae2d8a7f5a
carles-escude <75261579+carles-escude@users.noreply.github.com>
2023-11-13 22:28:06 +0100

    Primer commit

---
+++ index.8edf9a3c.js
@@ -0,0 +1,3 @@
+window.addEventListener("load",function(){window.cookieconsent.initialise({palette:{popup:{background:"#000"},button:{background:"#1DB954"}},content:{message:"Aquest lloc web utilitza galetes per assegurar que obtingui la millor experi\xe8ncia al nostre lloc web.",dismiss:"Accepta",link:"M\xe9s informaci\xf3",href:"#"}})});//# sourceMappingURL=index.8edf9a3c.js.map
+
+//# sourceMappingURL=index.8edf9a3c.js.map
