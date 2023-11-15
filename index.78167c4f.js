779ebb1e316b2a0a35d4606fd2c9200a5754721d
carles-escude <75261579+carles-escude@users.noreply.github.com>
2023-11-15 22:36:08 +0100

    wip

--- index.78167c4f.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("load",function(){window.cookieconsent.initialise({palette:{popup:{background:"#000"},button:{background:"#1DB954"}},content:{message:"Aquest lloc web utilitza galetes per assegurar que obtingui la millor experi\xe8ncia al nostre lloc web.",dismiss:"Accepta",link:"M\xe9s informaci\xf3",href:"#"}})});//# sourceMappingURL=index.78167c4f.js.map
-
-//# sourceMappingURL=index.78167c4f.js.map
--- index.e00c3d66.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("load",function(){window.cookieconsent.initialise({palette:{popup:{background:"#000"},button:{background:"#1DB954"}},content:{message:"Aquest lloc web utilitza galetes per assegurar que obtingui la millor experi\xe8ncia al nostre lloc web.",dismiss:"Accepta",link:"M\xe9s informaci\xf3",href:"#"}})});//# sourceMappingURL=index.e00c3d66.js.map
-
-//# sourceMappingURL=index.e00c3d66.js.map
